6be02c72f83a2509fe724929f1e1dc6e6fdbeacd
eusoubrasileiro <eusoubrasileiro@gmail.com>
2012-02-16 22:22:04 -0200

    Implicit 1D seams to be ok showing energy decay with time, although I dont know how to properly set the initial conditions. Implicit 2D changed to Crack Nickelson but dont know if its working. Nice 1D animations done.

--- source/python/WaveFd/WaveAnim.py
+++ source/python/WaveFd/WaveAnim.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pylab as py
 from os import system
+from matplotlib import cm
+from pylab import Axes
 
 
 def _Clear(name, imfmt):
@@ -30,27 +32,75 @@ def _AviFromPng(name):
     something like 
     aviconvert = "ffmpeg -i +" name + "*.png " + name ".avi" 
     """
-    aviconvert = "ffmpeg -i " + name + "%03d.png " + name + ".avi" 
+    aviconvert = "ffmpeg -i " + name+"%03d.png " + name + ".avi" 
     
     system(aviconvert)
     print aviconvert
     
     return
+    
+    
+def Wave1DField(ufield, vel, ds=None, spaceaxis=None, extent=None):
+    """
+    Draw a 1D wavefield at some instant of time
+    As background is used the vel field (same dimension as ufield)
+    ufield - displacement field at an instant of time
+    vel - velocity field
+    ds space increment (optional if spaceaxis is set) 
+    spaceaxis (distance for each index of ufield)
+    extent (xmin, xmax, ymin, ymax) - to use to limits the plots
+    """
+    
+    if ds == None and spaceaxis == None:
+        print "nothing to do!"
+        return
+    
+    # max index space
+    sm = np.size(ufield)
+    
+    if spaceaxis == None:
+        # time axis and space axis
+        spaceaxis = np.arange(0, sm*ds, ds)
+        
+    if extent == None:
+        # put the limits and position of the image to be plotted
+        xmin, xmax, ymin, ymax = min(spaceaxis), max(spaceaxis), min(ufield), max(ufield)
+        extent= xmin, xmax, ymin, ymax
+    
+    # plot initial curve
+    py.plot(spaceaxis, ufield,'w-',linewidth=2)
+    py.xlabel("distance (m)")
+    
+    # create velocity overlay to plot behind
+    velbar = np.zeros((2,sm))
+    velbar[:] = vel
+    
+    # optional cmap=cm.jet, apect='auto' adjust aspect to the previous plot
+    py.imshow(velbar, interpolation='bilinear', extent=extent, alpha=0.7, origin='lower', aspect='auto')
+    
+    cb = py.colorbar(orientation='horizontal')
+    cb.set_label("velocity (m/s)")
+    
+    # just to certify
+    py.axis(extent)
+    
+    py.show()
+    
+    
+    
 
-
-def Wave1DAnim(snapshots, name='Wave1DAnim', bckg=None, anim="gif", Ds=1, Dt=0.01, frameint=10):
+def Wave1DAnim(snapshots, name='Wave1DAnim', vel=None, anim="gif", ds=1, dt=0.01, frameint=10):
     """
     snapshot is a 2d matrix [time][frames]
     anim = animation tipe (gif or avi)
     name is the file name of the animation image
-    bckg is something else to plot as background (like velocity)  
-    Ds space in x axis (1 meter)
-    Dt time increment between simulation steps (0.01 second)
+    vel is the background velocity  
+    ds space in x axis (1 meter)
+    dt time increment between simulation steps (0.01 second)
     frameint is in miliseconds  = the interval between frames
     """
     #clear before creating new files
     _Clear(name,"png")
-    _Clear(name, anim)
     
     py.ion()
     
@@ -58,27 +108,43 @@ def Wave1DAnim(snapshots, name='Wave1DAnim', bckg=None, anim="gif", Ds=1, Dt=0.0
     tm = np.shape(snapshots)[0]
     sm = np.shape(snapshots)[1]
     # time axis and space axis
-    timeaxis = np.arange(0,tm*Dt, Dt)
-    spaceaxis = np.arange(0, sm*Ds, Ds)
+    timeaxis = np.arange(0,tm*dt, dt)
+    spaceaxis = np.arange(0, sm*ds, ds)
     
     # get the maximum and minimum y value to not blow the scale
-    mx = mn = 0.0
+    # during the movie
+    ymax = ymin = 0.0
     for i in range(tm):
-        mxi = max(snapshots[i])
-        mni = min(snapshots[i]) 
-        if( mxi > mx ):
-            mx = mxi
-        if( mni < mn ):
-            mn = mni
+        ymaxi = max(snapshots[i])
+        ymini = min(snapshots[i]) 
+        if( ymaxi > ymax ):
+            ymax = ymaxi
+        if( ymini < ymin ):
+            ymin = ymini
+    
+    # extents of the picture
+    xmin, xmax = min(spaceaxis), max(spaceaxis) 
+    extent= xmin, xmax, ymin, ymax
+    
+    # font position
+    width = xmax-xmin
+    height = ymax-ymin
+    posx = 0.8*width+xmin
+    posy = 0.8*height+ymin
+    # not working?
+    # verticalalignment='top', 
+    # horizontalalignment='right',
     
     for t in range(tm):
-        py.plot(spaceaxis, snapshots[t])
-        
-        if bckg != None: # plot background
-            py.plot(spaceaxis, bckg)
-        
-        py.axis(ymax=mx, ymin=mn) # set axis ranges
-        py.text(0.7*sm*Ds, 0.7*mx, "{0:1.5f}".format(timeaxis[t])) # draw time
+        Wave1DField(snapshots[t], vel, spaceaxis=spaceaxis, extent=extent)
+        # set axis ranges
+        py.axis(extent) 
+        # draw time
+        py.text(posx, posy, 
+                "{0:1.5f}".format(timeaxis[t]),
+                backgroundcolor='w',
+                style='italic',
+                color='b') 
         # since its just math objects would be perfect
         # will be something like Wave1DAnim001.png
         py.savefig(name+"{0:03d}".format(t)+'.png', dpi=50)
@@ -92,6 +158,8 @@ def Wave1DAnim(snapshots, name='Wave1DAnim', bckg=None, anim="gif", Ds=1, Dt=0.0
         
     if ( anim == "avi"):
         _AviFromPng(name)
-    
+        _Clear(name, "png")
+        
     #clear after creating new files
     #_Clear(name,"png")
+    
