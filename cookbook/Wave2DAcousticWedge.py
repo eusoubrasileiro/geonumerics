09d1e32d7abe738bb0612f73f7ef9bcdb27f10d0
eusoubrasileiro <eusoubrasileiro@gmail.com>
2013-07-16 14:29:47 -0300

    Started working on fatiando github and module for segy reading. Implemented one initial version that is working and functional (compressed in psegy.tar.gz) but is not good enough compared to Obspy. Obspy has many developers, very good documentation and very flexible class with broad support to segy and even su files. Really really recommended for segy manipulation if needed in the future. Due that reason Segy directory removed.

--- cookbook/Wave2DAcousticWedge.py
+++ cookbook/Wave2DAcousticWedge.py
@@ -8,15 +8,14 @@ after save it as an avi animation
 import sys
 sys.path.append('../source/python');
 
+import numpy as np
+import PIL
 from WaveFd.Imp2DLuWave import Imp2DLuSparseWave
 from WaveFd import WaveAnim
 from WaveFd.Wavelet import Triangle
-import numpy
-from PIL import Image
-import numpy as np
 # loading velocity from wedge image file
 filename='Wedge160x400.png'
-img = Image.open(filename)
+img = PIL.Image.open(filename)
 img.load()
 img = img.convert('I') # gray scale, convert format
 velocity  = np.asarray(img, dtype=np.float32)
@@ -29,6 +28,6 @@ triangle = Triangle(100.0, samplerate)
 wd2d = Imp2DLuSparseWave(400, 160, 10, samplerate, velocity, 225, 0, 1000, wavelet=triangle)
 movie= wd2d.Simulate()
 del wd2d # just cleaning up
-movie = numpy.save('Imp2DWedge_400x160x1000.npy', movie) # just for preservation save it also
-movie = numpy.load('Imp2DWedge_400x160x1000.npy')
+movie = np.save('Imp2DWedge_400x160x1000.npy', movie) # just for preservation save it also
+movie = np.load('Imp2DWedge_400x160x1000.npy')
 WaveAnim.Wave2DAnim(movie, 10, 0.001, velocity, vmin=-0.002, vmax=0.002, filename='Imp2DWedge_400x160x1000')
